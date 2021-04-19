12016f8862d50cb1bfd24edaa43e2b1b5e62e5c0
MIRKO TRECCARICHI <uni400949@studium.unict.it>
2021-04-19 22:56:35 +0200

    minor changes

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ let url = 'https://newsapi.org/v2/everything?' +
     'q=slow food&' +
     'from=' + date_ + '&' +
     'language=it&' +
-    //'sortBy=popularity&' +
+    'sortBy=popularity&' +
     'apiKey=c9b8835b7f2c419982ddc0396b5944cc';
 
 let req = new Request(url);
